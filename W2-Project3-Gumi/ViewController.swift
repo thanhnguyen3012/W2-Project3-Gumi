08456254e03894438b50780d02150f3b6fa94113
Nguyen <nguyennguyen121198@gmail.com>
2021-03-26 09:27:16 +0700

    Initial Commit

---
+++ W2-Project3-Gumi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W2-Project3-Gumi
+//
+//  Created by Thành Nguyên on 26/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
